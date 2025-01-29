c53f0ac152392fc38f166efc04c1b9c19c3de182
Roberto Barbagallo <developer@greenleafstudio.it>
2025-01-29 13:45:20 +0700

    logo size

--- storefront/src/modules/layout/templates/logo.tsx
+++ storefront/src/modules/layout/templates/logo.tsx
@@ -5,7 +5,7 @@ export default  function Logo() {
         <div className="w-[120px]">
             <div className="bg-black border-2 border-black">
                 <div className="flex justify-center border-2 border-white m-[4px]">
-                    <div className="flex flex-col justify-center pt-[2px] pb-[6px] text-center">
+                    <div className="flex flex-col justify-center pb-[3px] text-center">
                         <p className="text-white m-0 w-full relative font-dye leading-none text-[32px]">Amara</p>
                         <p className="text-white m-0 w-full relative font-dye leading-none text-[24px]">Beer Lab</p>
                     </div>
